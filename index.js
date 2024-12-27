8f667cffc67a17fe97c5458bfee0a6b8f2630494
Anyang Diing <anyang.ngor@student.moringaschool.com>
2024-12-27 23:23:07 +0300

    Scope Lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+// Declare a global variable `customerName`
+var customerName = 'bob';
+
+// Function to uppercase the `customerName`
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Function to set a global variable `bestCustomer`
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // `bestCustomer` is implicitly declared in global scope
+}
+
+// Function to overwrite `bestCustomer`
+function overwriteBestCustomer(newCustomer) {
+  bestCustomer = newCustomer;
+}
+
+// Declare a constant `leastFavoriteCustomer`
+const leastFavoriteCustomer = 'someone';
+
+// Function to attempt changing `leastFavoriteCustomer`
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new customer'; // This will throw an error
+}
